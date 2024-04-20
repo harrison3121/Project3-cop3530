d1b5e95b79b098f6cd6056e54b6023e815becc54
Zhengxiao Wang <139068347+ShawnWang21@users.noreply.github.com>
2024-04-20 15:20:30 -0400

    New version of internal adjacency List
    with basic functions
    -insert to graph
    -Search single game
    -return top three platforms

---
+++ AdjacencyList.h
@@ -0,0 +1,122 @@
+#pragma once
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+#include "GraphAdjList.h"
+#include "DataSource.h"
+#include "Bridges.h"
+#include "Array1D.h"
+
+using namespace std;
+using namespace bridges;
+
+struct GameNode {
+    string title;
+    vector<string> platforms;
+    float rating;
+    vector<string> genres;
+    GameNode(string title, float r, vector<string> g) : title(title), rating(rating), genres(genres) {}
+    //platform vector later gets pushed in when adding games
+};
+class AdjacencyList
+{
+private:
+    int counter;
+    map<string, vector<string>> platform_map;//map for platform value:vector of game names offered on the platform
+    map<string, vector<string>> genre_map;//map for genre value: count of games in each genre
+    //map<int, GameNode*> game_i;//map for game with key as int representation of game
+    map<string, GameNode*> game_s;//map for game with key as string and value as int representation
+    map<string, vector<pair<string,int>>> I_graph;//internal graph
+public:
+    void incertLink(Game game);
+    vector<string> TopThreePlatform(int option);
+    void searchGame(string gameName);
+    AdjacencyList() { counter = 0; }
+};
+void AdjacencyList::incertLink(Game game)
+{
+    if (platform_map.find(game.getPlatformType()) == platform_map.end())
+    {
+        vector<string> temp = { game.getTitle() };
+        platform_map.insert(make_pair(game.getPlatformType(), temp));//add new platform
+    }
+    else
+    {   
+        platform_map[game.getPlatformType()].push_back(game.getTitle());//increment game count for this platform
+    }
+    for (string genre : game.getGameGenre())
+    {
+        if (genre_map.find(genre) == genre_map.end())
+        {
+            vector<string> temp = { game.getTitle() };
+            genre_map.insert(make_pair(genre, temp));//add new genre
+        }
+        else
+        {
+            genre_map[genre].push_back(game.getTitle());//increment game count for this genre
+        }
+    }
+    if (game_s.find(game.getTitle()) == game_s.end())
+    {
+        counter++;
+        game_s.insert(make_pair(game.getTitle(), new GameNode(game.getTitle(), game.getRating(),game.getGameGenre())));//add game node
+        game_s[game.getTitle()]->platforms.push_back(game.getPlatformType());//put in the platform for this game
+    }
+    else
+    {
+        game_s[game.getTitle()]->platforms.push_back(game.getPlatformType());//add addition platform to this game
+
+    }
+    for (string gm : platform_map[game.getPlatformType()])
+    {
+        if (I_graph.find(game.getTitle()) == I_graph.end())
+        {
+            I_graph[game.getTitle()].push_back(make_pair(gm, 5));
+        }
+        
+    }
+}
+vector<string> AdjacencyList::TopThreePlatform(int option)
+{
+    map<int, string> rank;
+    vector<string> rtn;
+    if (option != 1 || option != 2 || option != 3)
+    {
+        cout << "warning message" << endl;
+        return {};
+    }
+    if (option == 1)
+    {
+        //most games
+        for (pair<string, vector<string>> pfpair : platform_map)
+        {
+            rank.insert(make_pair(pfpair.second.size(),pfpair.first));//fix me: platform with same amount of games
+        }
+
+    }
+    int i = 0;
+    for (pair<int, string> rtpair : rank)
+    {
+        if (i > 2)
+            break;
+        rtn.push_back(rtpair.second);
+    }
+    return rtn;
+}
+void AdjacencyList::searchGame(string gameName)
+{
+    if (game_s.find(gameName) == game_s.end())
+    {
+        cout << "Game doesn't exist in this dataset" << endl;
+        return;
+    }
+    else
+    {
+        cout << gameName << " exist in the dataset, and offered on " << game_s[gameName]->platforms.size() << " platforms, which are " << endl;
+        for (string pf : game_s[gameName]->platforms)
+        {
+            cout << pf << endl;
+        }
+    }
+}
